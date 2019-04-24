04016df01f98f6f6bfa958d6c44a703d209bd515
Eden Yeung <basktballer@gmail.com>
2019-04-24 23:11:52 +0000

    Register route working, querying form data into string, pre merge with V code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,36 @@ $(() => {
     url: "/api/users"
   }).done((users) => {
     for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      $("<div>").text(`${user.first_name}, ${user.last_name}`).appendTo($("body"));
     }
   });;
+});
+
+$(document).ready(function() {
+  // $("#register-button").on("click", function () {
+  //   $(".new-tweet").slideToggle("fast");
+  //   if ($(".new-tweet").is(':visible')) 
+  //   {
+  //     $(".tweet-area").focus();
+  //   }
+  // });
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    const userInput =  $(this).serialize();
+    console.log(userInput);
+      $.ajax({
+        type: 'POST',
+        url: "/api/users/register",
+        data: userInput
+      })
+      .done ( () => {
+        console.log(`${userInput}, Tweet uploaded`);
+        $(".tweet-area").val("");
+        $( ".tweet-area" ).trigger( "input", [ "" ] );
+        autoRenderNewTweet();
+      })
+      .fail ( () => {
+        console.log("Tweet upload failed.");
+      })    
+    });
 });
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,17 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  router.post("/register", (req, res) => {
+    const userDetailsArr = [{id: 4, first_name: 'DummyFirst', last_name: 'User' , username: 'testusername', email: 'email@email.com' , password: '123' }]
+    knex('users')
+      .insert(userDetailsArr)
+      .then(() => console.log(`Person added. First Name: ${userDetailsArr[0]}, Last Name: ${userDetailsArr[1]}, username: ${userDetailsArr[2]}, password: ${userDetailsArr[4]}`))
+      .catch((err) => { console.log(err); throw err })
+      .finally(() => {
+          knex.destroy();
+      });        
+  });
+
   router.get("/", (req, res) => {
     knex
       .select("*")
@@ -13,6 +24,8 @@ module.exports = (knex) => {
         res.json(results);
     });
   });
+
+
 
   return router;
 }

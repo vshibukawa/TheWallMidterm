7230f68137161e608c1c9dd78d6721c9daeb4699
Karl Chvojka <karl.chvojka@gmail.com>
2019-04-24 22:59:11 +0000

    Initial steps for front end views, font inclusions, etc.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,18 @@ $(() => {
     }
   });;
 });
+
+$(function(){
+  $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+  });
+
+  $(window).resize(function(e) {
+    if($(window).width()<=768){
+      $("#wrapper").removeClass("toggled");
+    }else{
+      $("#wrapper").addClass("toggled");
+    }
+  });
+});

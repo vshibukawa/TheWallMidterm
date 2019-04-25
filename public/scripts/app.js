7534961351d1edf685eefbfcffb898cf1d7ad8a2
Karl Chvojka <karl.chvojka@gmail.com>
2019-04-25 15:34:11 +0000

    Feature profile form populates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,6 +14,16 @@ $(document).ready(function() {
     $('#popup_profile').css('display', 'none');
   })
 
+  let userInfo = {
+    id: 15,
+    first_name: 'Karl',
+    last_name: 'Chvojka',
+    username: 'karl.chvojka',
+    email: 'karl.chvojka@gmail.com',
+    password: 'thingsandstuff',
+    avatar: 'https://avatars2.githubusercontent.com/u/4898500?s=460&v=4'
+  }
+
   // Handle Form Submit
   $("form").on("submit", function(event) {
     event.preventDefault();
@@ -35,6 +45,14 @@ $(document).ready(function() {
       })
   });
 
+  $(function profilePopulate () {
+    $('#popup_profile input[name=first_name]').val(userInfo.first_name);
+    $('#popup_profile input[name=last_name]').val(userInfo.last_name);
+    $('#popup_profile input[name=username]').val(userInfo.username);
+    $('#popup_profile input[name=email]').val(userInfo.email);
+    $('#popup_profile input[name=password]').val(userInfo.password);
+    $('#popup_profile input[name=avatar]').val(userInfo.avatar);
+  });
   // THIS FUNCTION IS FOR THE MENU TOGGLE SPECIFICALLY  //
   // ONLY CHANGE WHEN ADDING COLLAPSING SIDEBAR STRETCH //
 

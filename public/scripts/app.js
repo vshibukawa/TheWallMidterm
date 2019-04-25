cf9ef43b6a079ac8a53da3ca96ab0dc5ed37ee46
Karl Chvojka <karl.chvojka@gmail.com>
2019-04-25 19:05:06 +0000

    Built out js declarations of resource minimal structure

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,42 @@
+
+function createResourceElement (input) {
+  // Create variables representing the individual elements in a resource.
+  // Resource wrap creation.
+  let resWrap = $('<div>').addClass('col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12');
+
+  //Resource Inner Wrap creation
+  let resInner = $('<div>').addClass('resource_mininmal_single mr-1 m1-1');
+
+  //Resource Inner Top creation
+  let resInnerHead = $('<div>').addClass('resource_min_top_wrap');
+  let resInnerHeadTitle = $('<h3>');
+  let resInnerHeadLink = $('<a>');
+  let resInnerHeadDesc = $('<p>');
+  let resInnerHeadCatTitle = $('<h4>');
+  let resInnerHeadCat = $('<p>');
+
+  //Resource Social Wrap
+  let resInnerSoc = $('<div>').addClass('resource_minimal_social_wrap row');
+  let resInnerSocLikes = $('<div>').addClass('col-3');
+  let resInnerSocRate = $('<div>').addClass('col-3');
+  let resInnerSocCom = $('<div>').addClass('col-3');
+  let resInnerSocUser = $('<div>').addClass('col-3');
+
+  // ****************************************************** //
+  // Appending elements to facilitate the creation of a Resource.
+  // ****************************************************** //
+
+  // Create Resource wrapper
+}
+
+
+
+function renderResources (inputData) {
+  for (let resource of inputData) {
+    $('.main_section_wrap').prepend(createResourceElement(resource))
+  }
+}
+
 $(document).ready(function() {
   // Handle registration showing JS
   $("#register_button").click(function () {
@@ -45,6 +84,8 @@ $(document).ready(function() {
       })
   });
 
+
+  // PROFILE PAGE FORM ON LOAD POPULATE FIELDS
   $(function profilePopulate () {
     $('#popup_profile input[name=first_name]').val(userInfo.first_name);
     $('#popup_profile input[name=last_name]').val(userInfo.last_name);
@@ -53,6 +94,21 @@ $(document).ready(function() {
     $('#popup_profile input[name=password]').val(userInfo.password);
     $('#popup_profile input[name=avatar]').val(userInfo.avatar);
   });
+
+  $(function pagePopulate () {
+    let resourcesMain;
+    $.ajax({
+      type: 'GET',
+      url: 'api/resources/?limit=20'
+    })
+    .done( (data) => {
+      renderResources(data);
+    })
+    .fail( (err) => {
+      console.log('Failed', err)
+    })
+  })
+
   // THIS FUNCTION IS FOR THE MENU TOGGLE SPECIFICALLY  //
   // ONLY CHANGE WHEN ADDING COLLAPSING SIDEBAR STRETCH //
 
